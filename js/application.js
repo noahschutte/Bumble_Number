28cd3db28f698cc38d23c7b00ea8330a18ab766b
Noah Schutte <noahschutte@gmail.com>
2016-07-12 18:01:42 -0600

    add color to tiles

--- js/application.js
+++ js/application.js
@@ -8,38 +8,54 @@ boardView.renderBoard(board.grid);
 
   $(document).on('keyup', function(event) {
     event.preventDefault();
-    if(event.keyCode == 38) {
+    var newTile;
+    newTile = false;
+    if(event.keyCode === 38) {
+        if(board.canMoveUpToCombine() || board.canAnyTileMoveUp()) {
+            newTile = true;
+        }
       board.moveUpToCombine();
       board.removeTheBigO();
       board.moveUp();
-      nextMove();
+      nextMove(newTile);
     }
-    if(event.keyCode == 40) {
+    if(event.keyCode === 40) {
+        if(board.canMoveDownToCombine() || board.canAnyTileMoveDown()) {
+            newTile = true;
+        }
        board.moveDownToCombine();
        board.removeTheBigO();
        board.moveDown();
-       nextMove();
+       nextMove(newTile);
     }
-    if(event.keyCode == 37) {
+    if(event.keyCode === 37) {
+        if(board.canMoveLeftToCombine() || board.canAnyTileMoveLeft()) {
+            newTile = true;
+        }
       board.moveLeftToCombine();
       board.removeTheBigO();
       board.moveLeft();
-      nextMove();
+      nextMove(newTile);
     }
-    if(event.keyCode == 39) {
+    if(event.keyCode === 39) {
+        if(board.canMoveRightToCombine() || board.canAnyTileMoveRight()) {
+            newTile = true;
+        }
       board.moveRightToCombine();
       board.removeTheBigO();
       board.moveRight();
-      nextMove();
+      nextMove(newTile);
     }
     if(board.isBoardFull() && !board.canAnyTilesMoveToCombine()) {
       boardView.gameOver();
     }
   });
 
-var nextMove = function() {
+var nextMove = function(newTile) {
   if (!board.isBoardFull()) {
-  board.placeNewTile();
+      if (newTile === true) {
+          board.placeNewTile();
+      }
   boardView.renderBoard(board.grid);
   }
 };
--- js/view.js
+++ js/view.js
@@ -10,12 +10,40 @@ var BoardView = function() {
 };
 
 BoardView.prototype.renderBoard = function(grid) {
+    var currentTile;
   for (var r = 0; r < grid.length; r++) {
     for (var c = 0; c < grid[r].length; c++) {
+      currentTile = $('#boardView .row-' + r + ' .col-' + c);
       if (grid[r][c] === "X") {
-        $('#boardView .row-' + r + ' .col-' + c).text("");
+        currentTile.text("");
+        currentTile.css("background-color", "white")
       } else {
-        $('#boardView .row-' + r + ' .col-' + c).text(grid[r][c]).css("color","black");
+        currentTile.text(grid[r][c]);
+        if (currentTile.text() == 2) {
+            currentTile.css("background-color", "gray")
+        } else if (currentTile.text() == 4) {
+            currentTile.css("background-color", "yellow")
+        } else if (currentTile.text() == 8) {
+            currentTile.css("background-color", "orange")
+        } else if (currentTile.text() == 16) {
+            currentTile.css("background-color", "green")
+        } else if (currentTile.text() == 32) {
+            currentTile.css("background-color", "blue")
+        } else if (currentTile.text() == 64) {
+            currentTile.css("background-color", "purple")
+        } else if (currentTile.text() == 128) {
+            currentTile.css("background-color", "purple")
+        } else if (currentTile.text() == 256) {
+            currentTile.css("background-color", "silver")
+        } else if (currentTile.text() == 512) {
+            currentTile.css("background-color", "gold")
+        } else if (currentTile.text() == 1024) {
+            currentTile.css("background-color", "white")
+        } else if (currentTile.text() == 2048) {
+            currentTile.css("background-color", "white")
+        } else if (currentTile.text() == 4096) {
+            currentTile.css("background-color", "white")
+        }
       }
     }
   }
